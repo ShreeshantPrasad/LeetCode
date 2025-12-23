d3bbb152083f1d6e195f0d1f8c07f05b27893f4c
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2025-12-23 10:51:20 +0530

    Time: 44 ms (43.4%), Space: 46.2 MB (45.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,48 +1,48 @@
 class Solution {
-    // public int lengthOfLIS(int[] nums) {
-    //     int n=nums.length;
-    //     int[] dp=new int[n];
-    //     Arrays.fill(dp,1);
-    //     int ans=1;
-    //     for(int i=0;i<dp.length;i++){
-    //         for(int j=0;j<i;j++){
-    //             if(nums[i]>nums[j]){
-    //                 dp[i]=1+dp[j];
-    //             }
-    //         }
-    //         ans=Math.max(ans,dp[i]);
-    //     }
-    //     return ans;
-        
-    // }
-
     public int lengthOfLIS(int[] nums) {
-        Integer[][] dp = new Integer[nums.length][nums.length+1];
-        return solve(nums,0,-1,dp);
-    //     // int n = nums.length;
-    //     // int maxx = 1;
-        
-    //     // for (int i = 0; i < n; i++) {
-    //     //     int curr = 1;
-    //     //     int prev = i;
-    //     //     for (int j = i ; j < n; j++) {
-    //     //         if (nums[j] > nums[prev]) {
-    //     //             prev = j;
-    //     //             curr++;
-    //     //         }
-    //     //     }
-    //     //     maxx = Math.max(maxx, curr);
-    //     // }return maxx;
-
-    }
-    public int solve(int[] nums, int curr, int prev,Integer[][] dp){
-        if(curr == nums.length) return 0;
-        int take=0;
-        if(dp[curr][prev+1] != null) return dp[curr][prev+1];
-        if(prev==-1 || nums[curr]>nums[prev]){
-            take = 1 + solve(nums,curr+1,curr,dp);
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        int ans=1;
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],1+dp[j]);
+                }
+            }
+            ans=Math.max(ans,dp[i]);
         }
-        int notTake = solve(nums,curr+1,prev,dp);
-        return dp[curr][prev+1] = Math.max(take,notTake);
+        return ans;
+        
     }
+
+    // public int lengthOfLIS(int[] nums) {
+    //     Integer[][] dp = new Integer[nums.length][nums.length+1];
+    //     return solve(nums,0,-1,dp);
+    // //     // int n = nums.length;
+    // //     // int maxx = 1;
+        
+    // //     // for (int i = 0; i < n; i++) {
+    // //     //     int curr = 1;
+    // //     //     int prev = i;
+    // //     //     for (int j = i ; j < n; j++) {
+    // //     //         if (nums[j] > nums[prev]) {
+    // //     //             prev = j;
+    // //     //             curr++;
+    // //     //         }
+    // //     //     }
+    // //     //     maxx = Math.max(maxx, curr);
+    // //     // }return maxx;
+
+    // }
+    // public int solve(int[] nums, int curr, int prev,Integer[][] dp){
+    //     if(curr == nums.length) return 0;
+    //     int take=0;
+    //     if(dp[curr][prev+1] != null) return dp[curr][prev+1];
+    //     if(prev==-1 || nums[curr]>nums[prev]){
+    //         take = 1 + solve(nums,curr+1,curr,dp);
+    //     }
+    //     int notTake = solve(nums,curr+1,prev,dp);
+    //     return dp[curr][prev+1] = Math.max(take,notTake);
+    // }
 }
