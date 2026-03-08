90d301a8369eaffc39490d5e0e4b00290c9c8ff6
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-03-08 09:00:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -2,15 +2,16 @@ class Solution {
     int [] dp = new int[1001];
     int MOD = 1_000_000_007;
     public int numTilings(int n) {
-        Arrays.fill(dp,-1);
-        return solve(n);
-    }
-    private int solve(int n){
-        if(n == 1 || n == 2){
-            return n;
+        // if(n == 1 || n == 2){
+        //     return n;
+        // }
+        // if(n == 3) return 5;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i = 4; i <= n; i++){
+            dp[i] = ((2*dp[i-1]) % MOD + (dp[i-3]) % MOD) % MOD;
         }
-        if(n == 3) return 5;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = ((2*solve(n-1)) % MOD + (solve(n-3)) % MOD) % MOD;
+        return dp[n];
     }
 }
