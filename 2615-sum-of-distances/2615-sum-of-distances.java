14f035cacbbc9128f6a7fddb4771014ca96ec345
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-04-23 13:43:21 +0530

    Time: 66 ms (11.21%), Space: 131.7 MB (10.31%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long[] distance(int[] nums) {
+        long [] ans = new long[nums.length];
+        HashMap<Integer,Long> idxSum = new HashMap<>();
+        HashMap<Integer,Long> idxCount = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            long freq = idxCount.getOrDefault(nums[i], 0L);
+            long sum = idxSum.getOrDefault(nums[i], 0L);
+
+            ans[i] += freq*i - sum;
+
+            idxSum.put(nums[i],idxSum.getOrDefault(nums[i],0l) + (long) i);
+            idxCount.put(nums[i],idxCount.getOrDefault(nums[i],0l)+1);
+        }
+        idxSum.clear();
+        idxCount.clear();
+        for(int i = nums.length - 1; i >= 0; i--){
+            long freq = idxCount.getOrDefault(nums[i], 0L);
+            long sum = idxSum.getOrDefault(nums[i], 0L);
+
+            ans[i] += sum - freq*i;
+
+            idxSum.put(nums[i],idxSum.getOrDefault(nums[i],0l) + (long) i);
+            idxCount.put(nums[i],idxCount.getOrDefault(nums[i],0l)+1);
+        }
+        return ans;
+
+    }
+}
