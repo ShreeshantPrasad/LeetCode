8622776c8985abb7700a2760dbec4847882bdc90
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-06-16 14:53:11 +0530

    Time: 16 ms (65.82%), Space: 46.2 MB (82.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        Integer [][] dp = new Integer[n][amount+1];
-        int ans = solveRec(n-1, coins, amount,dp);
-        return (ans >= (int)1e9 ) ? -1 : ans;
-    }
-    public int solveRec(int n, int [] coins, int amount, Integer [][] dp){
-        if(amount == 0) return 0;
-        if(n==0){
-            if(amount % coins[0] == 0){
-                return amount/coins[0];
-            } 
-            else return (int)1e9;
+        int [] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int coin : coins){
+                if( i >= coin){
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
         }
-        if(dp[n][amount] != null) return dp[n][amount];
-        //Not take
-        int notTake = solveRec(n-1,coins,amount,dp);
-        //take
-        int take = (int)1e9;
-        if(coins[n]<=amount){
-            take = 1 + solveRec(n,coins,amount-coins[n],dp);
-        } 
-        return dp[n][amount] = Math.min(take, notTake);
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
