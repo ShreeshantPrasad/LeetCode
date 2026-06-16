2f48c16fb4ae1da336cfbb4db3fb1cbfa60a067f
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-06-16 18:53:13 +0530

    Time: 4 ms (76.47%), Space: 55.7 MB (20.59%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch == '*'){
+                if(ans.length() > 0) ans.deleteCharAt(ans.length() - 1);
+            }
+            else if( ch == '#'){
+                ans.append(ans.toString());
+            }
+            else if( ch == '%'){
+                ans.reverse();
+            }
+            else{
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
