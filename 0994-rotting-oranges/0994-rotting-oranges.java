f2f9cc60a37048bed9c56fe5e82e6d9b42e63647
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-07-20 16:05:12 +0530

    Time: 2 ms (91.08%), Space: 44.1 MB (72.8%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,19 +3,16 @@ class Solution {
     static int [] dr = {-1,1,0,0};
     static int [] dc = {0,0,-1,1};
     int maxTime = 0;
-
     public int orangesRotting(int[][] grid) {
         rowL = grid.length;
         colL = grid[0].length;
-        // var vis = new boolean[rowL][colL];
         int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
 
-        //add all rotten oranges into queue
         for(int i = 0; i<rowL; i++){
             for(int j = 0;j<colL; j++){
-                if(grid[i][j] == 2){ //rotten
-                    q.offer(new int[] {i,j,0});   //row,col,time
+                if(grid[i][j] == 2){ 
+                    q.offer(new int[] {i,j,0}); 
                 }
                 else if(grid[i][j] == 1) {
                     fresh++;
@@ -29,13 +26,10 @@ class Solution {
             int T = curr[2];
 
             maxTime = Math.max(maxTime,T);
-
-            // if(fresh==0) continue;
             for(int i = 0; i < 4;  i++){
                 int nRow = row + dr[i];
                 int nCol = col + dc[i];
 
-                //check valid and add to queue
                 if(nRow >= 0 && nCol >= 0 && nRow<rowL && nCol<colL && grid[nRow][nCol] == 1){
                     grid[nRow][nCol] = 2;
                     fresh--;
@@ -43,6 +37,7 @@ class Solution {
                 }
             }
         }
-        return fresh==0 ? maxTime : -1;
+        return (fresh == 0) ? maxTime : -1;
     }
+    
 }
