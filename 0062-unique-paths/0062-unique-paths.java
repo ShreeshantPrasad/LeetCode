b7dda50c928a3a1e9202c7e220cf436ff997cdec
Shreeshant <115723776+ShreeshantPrasad@users.noreply.github.com>
2026-09-17 15:30:27 +0530

    Time: 0 ms (100%), Space: 42.5 MB (12.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,19 @@
 class Solution {
-    int M,N;
-    Integer[][] dp;
+    int M, N;
+    Integer [][] dp;
     public int uniquePaths(int m, int n) {
         M = m;
         N = n;
-        dp = new Integer[m+1][n+1];
+        dp = new Integer[m][n];
         return help(0,0);
-
     }
     private int help(int i, int j){
         if(i == M-1 && j == N-1) return 1;
-        if(i < 0 || i >= M || j < 0 || j >= N) return 0;
+        if(i >= M || j >= N) return 0;
         if(dp[i][j] != null) return dp[i][j];
+
         int right = help(i,j+1);
         int down = help(i+1,j);
-
         return dp[i][j] = right+down;
     }
-
 }
